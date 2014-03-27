bc331d8ee5450a8b1d80aa3f35f15200a7da1bb3
Keerthik <keerthiko@gmail.com>
2014-03-27 03:43:30 -0700

    Added lots of styling and rules

---
+++ Webstuff/data/ruleset_v07_keerthik.js
@@ -0,0 +1,12 @@
+var data_07_ko = {
+	puppet: {
+		'name': 'Tormented Puppet',
+		'move': 2,
+		'melee': ['Explosion', 'LeapDeath'] 
+	},
+	duchess: {
+		'name': 'Duchess of Torment',
+		'move': 2,
+		'melee': ['Whiplash', 'Sadism'] 
+	},
+};
